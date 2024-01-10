c66aea32480a74668d07779dfbfb00b6898d1c4e
EmilAarup <emaa0001@stud.kea>
2024-01-10 03:20:10 +0100

    nye

--- menu_test/prøver.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("rotating-container").addEventListener("mouseover", function () {
-  this.classList.add("rotated");
-});
-
-document.getElementById("rotating-container").addEventListener("mouseout", function () {
-  this.classList.remove("rotated");
-});
